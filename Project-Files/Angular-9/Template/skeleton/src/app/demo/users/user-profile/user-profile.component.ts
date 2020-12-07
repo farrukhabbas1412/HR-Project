432e1f7ef9ee11b065846c47f0ab76630baca045
muhammadfahad <fahadkhan1229@gmail.com>
2020-12-08 01:13:51 +0500

    Editable header added

--- Project-Files/Angular-9/Template/skeleton/src/app/demo/users/user-profile/user-profile.component.ts
+++ Project-Files/Angular-9/Template/skeleton/src/app/demo/users/user-profile/user-profile.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {IAlbum, IEvent, Lightbox, LIGHTBOX_EVENT, LightboxConfig, LightboxEvent} from 'ngx-lightbox';
 import {Subscription} from 'rxjs';
 import {DomSanitizer} from '@angular/platform-browser';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-user-profile',
@@ -23,6 +24,9 @@ export class UserProfileComponent implements OnInit {
   public albums: Array<IAlbum>;
   private subscription: Subscription;
   headers: any;
+  headerForm = new FormGroup({
+    title: new FormControl()
+  });
 
   constructor(private lightbox: Lightbox, private lightboxEvent: LightboxEvent, private lighboxConfig: LightboxConfig, private sanitizer: DomSanitizer) {
     this.activeTab = 'home';
@@ -35,12 +39,7 @@ export class UserProfileComponent implements OnInit {
 
     this.editOtherInfo = false;
     this.editOtherInfoIcon = 'icon-edit';
-    this.headers = [
-      {'id': '1', name: 'First'},
-      {'id': '2', name: 'second'},
-      {'id': '3', name: 'Third'},
-      {'id': '4', name: 'Fourth'},
-    ]
+    this.headers = ['First', 'Fourth', 'Fifth']
     this.albums = [];
     for (let i = 1; i <= 6; i++) {
       const album = {
@@ -72,4 +71,7 @@ export class UserProfileComponent implements OnInit {
     }
   }
 
+  addHeader() {
+    this.headers.push(this.headerForm.controls.title.value)
+  }
 }
--- Project-Files/Angular-9/Template/skeleton/src/app/demo/users/user-profile/user-profile.module.ts
+++ Project-Files/Angular-9/Template/skeleton/src/app/demo/users/user-profile/user-profile.module.ts
@@ -6,17 +6,19 @@ import { UserProfileComponent } from './user-profile.component';
 import {SharedModule} from '../../../theme/shared/shared.module';
 import {NgbCarouselModule, NgbDropdownModule, NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
 import {LightboxModule} from 'ngx-lightbox';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [UserProfileComponent],
-  imports: [
-    CommonModule,
-    UserProfileRoutingModule,
-    SharedModule,
-    NgbDropdownModule,
-    NgbTooltipModule,
-    NgbCarouselModule,
-    LightboxModule
-  ]
+    imports: [
+        CommonModule,
+        UserProfileRoutingModule,
+        SharedModule,
+        NgbDropdownModule,
+        NgbTooltipModule,
+        NgbCarouselModule,
+        LightboxModule,
+        MatIconModule
+    ]
 })
 export class UserProfileModule { }
